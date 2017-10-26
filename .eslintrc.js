1bc7e1e1946fa7855f3f8cba6f43f2bb4fb26130
Thulio Ferraz Assis <contato@thulioassis.com.br>
2017-10-26 16:24:29 -0700

    Added initial configuration for the ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'max-len': ['warn', 240],
+    'no-bitwise': 'off',
+    'comma-dangle': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'import/extensions': ['error', 'always', {
+      js: 'never'
+    }],
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js']
+    }],
+    'import/prefer-default-export': 'off',
+    'no-alert': 'off',
+    'no-mixed-operators': 'off',
+    'no-prototype-builtins': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off'
+  }
+};
