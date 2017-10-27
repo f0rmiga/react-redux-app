55d0c3aa844819ed039e51f01d4c4d06d63b884f
Thulio Ferraz Assis <contato@thulioassis.com.br>
2017-10-26 17:27:27 -0700

    Renamed index.js to index.jsx and fixed linter errors and small bugs, also refactored it to separate into more components

---
+++ src/app/client/index.jsx
@@ -0,0 +1,50 @@
+import FarceActions from 'farce/lib/Actions';
+import BrowserProtocol from 'farce/lib/BrowserProtocol';
+import createHistoryEnhancer from 'farce/lib/createHistoryEnhancer';
+import queryMiddleware from 'farce/lib/queryMiddleware';
+import createConnectedRouter from 'found/lib/createConnectedRouter';
+import createMatchEnhancer from 'found/lib/createMatchEnhancer';
+import createRender from 'found/lib/createRender';
+import foundReducer from 'found/lib/foundReducer';
+import Matcher from 'found/lib/Matcher';
+import resolver from 'found/lib/resolver';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, compose, createStore } from 'redux';
+import logger from 'redux-logger';
+
+import routeConfig from '../routeConfig.jsx';
+
+import ErrorComponent from './components/Error.react.jsx';
+
+const store = createStore(
+  combineReducers({
+    found: foundReducer,
+  }),
+  compose(
+    createHistoryEnhancer({
+      protocol: new BrowserProtocol(),
+      middlewares: [
+        queryMiddleware,
+        logger,
+      ],
+    }),
+    createMatchEnhancer(new Matcher(routeConfig))
+  )
+);
+
+store.dispatch(FarceActions.init());
+
+const ConnectedRouter = createConnectedRouter({
+  render: createRender({
+    renderError: ErrorComponent,
+  }),
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter resolver={resolver} />
+  </Provider>,
+  document.getElementById('root'),
+);
