e9f268ef447ad91fed31e5a924fa838101b10925
Thulio Ferraz Assis <contato@thulioassis.com.br>
2017-10-31 15:10:05 -0700

    Renamed from Menu to NavBar

--- src/app/client/components/App.react.jsx
+++ src/app/client/components/App.react.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Menu from './Menu.react.jsx';
+import NavBar from './NavBar.react.jsx';
 
 function App(props) {
   return (
     <div>
-      <Menu />
+      <NavBar />
 
       {props.children}
     </div>
