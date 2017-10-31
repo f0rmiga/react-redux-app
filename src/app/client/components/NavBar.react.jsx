498edd4c05b961070313fd722e6cf170d08c393f
Thulio Ferraz Assis <contato@thulioassis.com.br>
2017-10-31 15:32:18 -0700

    Added a title and conditional rendering for the adding button in the NavBar

--- src/app/client/components/NavBar.react.jsx
+++ src/app/client/components/NavBar.react.jsx
@@ -6,21 +6,35 @@ function Menu() {
     marginBottom: '15px'
   };
 
+  const titleStyle = {
+    color: 'white',
+    padding: '10px 30px'
+  };
+
   return (
     <nav className="navbar is-dark" style={style} aria-label="main navigation">
+      <div className="navbar-start">
+        <p className="title" style={titleStyle}>Message app</p>
+      </div>
       <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="field is-grouped">
-            <p className="control">
-              <LinkItem className="button is-success" to="/newmessage">
-                <span className="icon">
-                  <i className="fa fa-plus" aria-hidden="true" />
-                </span>
-                <span>New message</span>
-              </LinkItem>
-            </p>
+
+        {window.location.pathname.match(/(\/messages\/)[0-9]+/g) &&
+
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <LinkItem className="button is-success" to="/newmessage">
+                  <span className="icon">
+                    <i className="fa fa-plus" aria-hidden="true" />
+                  </span>
+                  <span>New message</span>
+                </LinkItem>
+              </p>
+            </div>
           </div>
-        </div>
+
+        }
+
       </div>
     </nav>
   );
